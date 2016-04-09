6d4622745ab3f0a82588ad2ecdd2127f70d889eb
bamaboipa <tuf30050@temple.edu>
2016-04-08 20:44:01 -0400

    Password Recovery

--- 3342_TermProject/PasswordRecover.aspx.designer.cs
+++ 3342_TermProject/PasswordRecover.aspx.designer.cs
@@ -31,13 +31,13 @@ namespace _3342_TermProject {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl divQuestion;
         
         /// <summary>
-        /// txtSecQuest control.
+        /// lblSecQuest control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtSecQuest;
+        protected global::System.Web.UI.WebControls.Label lblSecQuest;
         
         /// <summary>
         /// txtSecAnswer control.
